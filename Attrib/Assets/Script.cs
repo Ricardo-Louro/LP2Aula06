2c81ae36c8dd1d53ba8fa0632c8e3ac3ff08a96c
Ricardo Louro <ricardo.f.louro@gmail.com>
2023-10-24 14:30:40 +0100

    Completed Exercise 1

---
+++ Attrib/Assets/Script.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    [SerializeField] [Range(0,500)]
+    private int integer;
+}
