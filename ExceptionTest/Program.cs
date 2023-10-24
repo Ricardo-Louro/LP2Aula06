e07ac312edcddd409192621a49c4bf8608dd8d08
Ricardo Louro <ricardo.f.louro@gmail.com>
2023-10-24 15:00:44 +0100

    Add basis for ExceptionTest

---
+++ ExceptionTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ExceptionTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Insere um número inteiro: ");
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Numero inserido: {i}");
+        }
+    }
+}
